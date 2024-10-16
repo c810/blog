e16b9e35e3afc962aea76f1fef3e04eb11789dbf
lhplanet <752869332@qq.com>
2024-10-16 11:57:40 +0800

    blog vault backup: 2024-10-16 11:57:40

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -34,7 +34,7 @@ interface BreadcrumbOptions {
 
 const defaultOptions: BreadcrumbOptions = {
   spacerSymbol: "❯",
-  rootName: "Home",
+  rootName: "首页",
   resolveFrontmatterTitle: true,
   hideOnRoot: true,
   showCurrentPage: true,
